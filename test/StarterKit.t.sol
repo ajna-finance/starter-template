9cad80d850ef5b82e04d9001954cc38223123c96
Ian Harvey <iharvey@comcast.net>
2024-04-30 10:57:44 -0400

    finalized starterkit

--- test/StarterKit.t.sol
+++ test/StarterKit.t.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import { Test, console } from "forge-std/Test.sol";
 import { StarterKit } from "../src/StarterKit.sol";
 import { StarterKitTestHelperContract } from './Utils.t.sol';
+
 
 contract CounterTest is StarterKitTestHelperContract {
     StarterKit public counter;
@@ -11,6 +11,48 @@ contract CounterTest is StarterKitTestHelperContract {
     function setUp() public {
         counter = new StarterKit();
         counter.setNumber(0);
+        _mintCollateralAndApproveTokens(_bill, 100.0 * 1e18);
+        _mintQuoteAndApproveTokens(_liz, 10_000.0 * 1e18);
+
+    }
+
+    function testFillBook() public {
+
+        // example test showing how to fill the book.
+
+        _addLiquidity({
+            from:    _liz,
+            amount:  100.0 * 1e18,
+            index:   2_000,
+            lpAward: 99.995433789954337900 * 1e18,
+            newLup:  1004968987.606512354182109771 * 1e18
+        });
+
+        _addLiquidity({
+            from:    _liz,
+            amount:  100.0 * 1e18,
+            index:   1_995,
+            lpAward: 99.995433789954337900 * 1e18,
+            newLup:  1004968987.606512354182109771 * 1e18
+        });
+
+        _addLiquidity({
+            from:    _liz,
+            amount:  100.0 * 1e18,
+            index:   1_990,
+            lpAward: 99.995433789954337900 * 1e18,
+            newLup:  1004968987.606512354182109771 * 1e18
+        });
+
+        _drawDebt({
+            from:               _bill,
+            borrower:           _bill,
+            amountToBorrow:     100.0 * 1e18,
+            limitIndex:         7388,
+            collateralToPledge: 10.0 * 1e18,
+            newLup:             47957.822483856118103434 * 1e18
+        });
+
     }
 
     function test_Increment() public {
--- test/Utils.t.sol
+++ test/Utils.t.sol
@@ -1,25 +1,32 @@
-import '@std/Test.sol';
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+// import '@std/Test.sol';
+import "forge-std/Test.sol";
 
 import {
     MAX_FENWICK_INDEX,
     COLLATERALIZATION_FACTOR,
     _borrowFeeRate
-} from '@ajna-core/libraries/helpers/PoolHelper.sol';
+} from 'lib/ajna-core/src/libraries/helpers/PoolHelper.sol';
 
-import { Maths }             from '@ajna-core/libraries/internal/Maths.sol';
-import { Token }             from '@ajna-core-test/utils/Tokens.sol';
-import { ERC20Pool }         from '@ajna-core/ERC20Pool.sol';
-import { ERC20PoolFactory }  from '@ajna-core/ERC20PoolFactory.sol';
-import { ERC721PoolFactory } from '@ajna-core/ERC721PoolFactory.sol';
-import { PositionManager }   from '@ajna-core/PositionManager.sol';
-import { PoolInfoUtils }     from '@ajna-core/PoolInfoUtils.sol';
+import { Maths }             from 'lib/ajna-core/src/libraries/internal/Maths.sol';
+import { Token }             from 'lib/ajna-core/tests/forge/utils/Tokens.sol';
+import { ERC20Pool }         from 'lib/ajna-core/src/ERC20Pool.sol';
+import { ERC20PoolFactory }  from 'lib/ajna-core/src/ERC20PoolFactory.sol';
+import { ERC721PoolFactory } from 'lib/ajna-core/src/ERC721PoolFactory.sol';
+import { PositionManager }   from 'lib/ajna-core/src/PositionManager.sol';
+import { PoolInfoUtils }     from 'lib/ajna-core/src/PoolInfoUtils.sol';
 
-import { IPool }            from '@ajna-core/interfaces/pool/IPool.sol';
-import { IPoolErrors }      from '@ajna-core/interfaces/pool/commons/IPoolErrors.sol';
-import { IPositionManager } from '@ajna-core/interfaces/position/IPositionManager.sol';
+import { IPool }            from 'lib/ajna-core/src/interfaces/pool/IPool.sol';
+import { IPoolErrors }      from 'lib/ajna-core/src/interfaces/pool/commons/IPoolErrors.sol';
+import { IPositionManager } from 'lib/ajna-core/src/interfaces/position/IPositionManager.sol';
+
+import { IERC20PoolEvents } from 'lib/ajna-core/src/interfaces/pool/erc20/IERC20PoolEvents.sol';
+import { IPoolEvents }      from 'lib/ajna-core/src/interfaces/pool/commons/IPoolEvents.sol';
 
 
-abstract contract StarterKitTestUtils is Test {
+abstract contract StarterKitTestUtils is Test, IPoolEvents, IERC20PoolEvents {
 
     IPool             internal _pool;
     PoolInfoUtils     internal _poolUtils;
@@ -29,12 +36,13 @@ abstract contract StarterKitTestUtils is Test {
 
     ERC20PoolFactory internal _poolFactory;
 
-    address internal _minterOne;
-
     // mainnet address of AJNA token, because tests are forked
     address internal _ajna = 0x9a96ec9B57Fb64FbC60B423d1f4da7691Bd35079;
 
     IPositionManager internal _positionManager;
+
+    address internal _bill;
+    address internal _liz;
 
     constructor() {
         // vm.createSelectFork(vm.envString("ETH_RPC_URL"));
@@ -43,41 +51,33 @@ abstract contract StarterKitTestUtils is Test {
         _poolFactory = new ERC20PoolFactory(_ajna);
         _pool        = ERC20Pool(_poolFactory.deployPool(address(_collateral), address(_quote), 0.05 * 10**18));
         _poolUtils   = new PoolInfoUtils();
+
+        _positionManager = new PositionManager(_poolFactory, new ERC721PoolFactory(_ajna));
+
+        _bill = makeAddr("bill");
+        _liz = makeAddr("liz");
     }
 
-    // basic test wrapper functions here
+    // Add basic wrapper functions here
 
 }
 
 abstract contract StarterKitTestHelperContract is StarterKitTestUtils {
 
-    address         internal _owner;
-    address         internal _bidder;
-    address         internal _updater;
-    address         internal _updater2;
 
-    Token internal _collateralOne;
-    Token internal _quoteOne;
-    Token internal _collateralTwo;
-    Token internal _quoteTwo;
+    /*
+    Issues with importing existing test utilities due to mapping issues in forge.
+    See -> https://github.com/foundry-rs/foundry/issues/5447
 
-    constructor() {
-
-        vm.makePersistent(_ajna);
-
-        _owner  = makeAddr("owner");
-
-        _positionManager = new PositionManager(_poolFactory, new ERC721PoolFactory(_ajna));
-
-        _collateralOne = new Token("Collateral 1", "C1");
-        _quoteOne      = new Token("Quote 1", "Q1");
-        _collateralTwo = new Token("Collateral 2", "C2");
-        _quoteTwo      = new Token("Quote 2", "Q2");
-
-    }
+    No plans to use node modules or add a package.json to ajna-core therefore we will have to copy / reuse functions here.
+    See -> https://github.com/ajna-finance/ajna-core/blob/master/tests/forge/utils/DSTestPlus.sol
+    and -> https://github.com/ajna-finance/ajna-core/blob/master/tests/forge/unit/ERC20Pool/ERC20DSTestPlus.sol
+    For more examples.
+    */
 
     function _mintQuoteAndApproveTokens(address operator_, uint256 mintAmount_) internal {
         deal(address(_quote), operator_, mintAmount_);
+        console2.log("operator", operator_);
 
         changePrank(operator_);
         _quote.approve(address(_pool), type(uint256).max);
@@ -92,13 +92,55 @@ abstract contract StarterKitTestHelperContract is StarterKitTestUtils {
         _quote.approve(address(_pool), type(uint256).max);
     }
 
-    // calculate required collateral to borrow a given amount at a given limitIndex
-    function _requiredCollateral(uint256 borrowAmount, uint256 indexPrice) internal view returns (uint256 requiredCollateral_) {
-        // calculate the required collateral based upon the borrow amount and index price
+    function _addLiquidity(
+        address from,
+        uint256 amount,
+        uint256 index,
+        uint256 lpAward,
+        uint256 newLup
+    ) internal {
         (uint256 interestRate, ) = _pool.interestRateInfo();
-        uint256 newInterestRate = Maths.wmul(interestRate, 1.1 * 10**18); // interest rate multipled by increase coefficient
-        uint256 expectedDebt = Maths.wmul(borrowAmount, _borrowFeeRate(newInterestRate) + Maths.WAD);
-        requiredCollateral_ = Maths.wdiv(Maths.wmul(expectedDebt, COLLATERALIZATION_FACTOR), _poolUtils.indexToPrice(indexPrice));
+        uint256 feeRate_ = Maths.WAD - Maths.wdiv(interestRate, 365 * 3e18);
+        uint256 amountAdded = Maths.wmul(amount, feeRate_);
+        _addLiquidityWithPenalty(from, amount, amountAdded, index, lpAward, newLup);
+    }
+
+    // Adds liquidity validating the lender deposit fee
+    function _addLiquidityWithPenalty(
+        address from,
+        uint256 amount,
+        uint256 amountAdded,    // amount less fee
+        uint256 index,
+        uint256 lpAward,
+        uint256 newLup
+    ) internal {
+        changePrank(from);
+
+        vm.expectEmit(true, true, false, true);
+        emit AddQuoteToken(from, index, amountAdded, lpAward, newLup);
+        (uint256 returnLpAward, uint256 returnedAmountAdded) = _pool.addQuoteToken(amount, index, type(uint256).max);
+        assertEq(returnLpAward, lpAward);
+        assertEq(returnedAmountAdded, amountAdded);
+    }
+
+
+    function _drawDebt(
+        address from,
+        address borrower,
+        uint256 amountToBorrow,
+        uint256 limitIndex,
+        uint256 collateralToPledge,
+        uint256 newLup
+    ) internal {
+        uint256 collateralScale = ERC20Pool(address(_pool)).collateralScale();
+        changePrank(from);
+
+        if (newLup != 0) {
+            vm.expectEmit(true, true, false, true);
+            emit DrawDebt(from, amountToBorrow, (collateralToPledge / collateralScale) * collateralScale, newLup);
+        }
+
+        ERC20Pool(address(_pool)).drawDebt(borrower, amountToBorrow, limitIndex, collateralToPledge);
     }
 
 }
